8553b553e7d9f680dadd69c13d5d3660a021a362
Rocketman <abhinayshambharkar1@gmail.com>
2025-07-31 16:34:16 +0530

    add configs folder to keep mongodb connection separated

---
+++ configs/mongo.db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const connectToDb = () => {
+    //connect
+    mongoose.connect("mongodb://127.0.0.1:27017/nemb58")
+    .then(()=>console.log("db connected, move! move! move!"))
+    .catch((err)=>console.log(err));
+};
+
+module.exports = connectToDb;
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 app.use(express.json());
 const UserRouter = require("./routers/user.router");
+const connectToDb = require("./configs/mongo.db");
 
-//connect
-mongoose.connect("mongodb://127.0.0.1:27017/nemb58")
-.then(()=>console.log("db connected, move! move! move!"))
-.catch((err)=>console.log(err));
+connectToDb();
 
 app.use("/users", UserRouter);
 
