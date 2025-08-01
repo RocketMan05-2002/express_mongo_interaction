d0f9adedadb2d40df5e490bc8e3161964bc09aae
Rocketman <abhinayshambharkar1@gmail.com>
2025-08-01 23:55:12 +0530

    model error fix

--- configs/mongo.db.js
+++ configs/mongo.db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectToDb = () => {
-    //connect
-    mongoose.connect("mongodb://127.0.0.1:27017/nemb58")
-    .then(()=>console.log("db connected, move! move! move!"))
-    .catch((err)=>console.log(err));
+  //connect
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/nemb58")
+    .then(() => console.log("db connected, move! move! move!"))
+    .catch((err) => console.log(err));
 };
 
 module.exports = connectToDb;
