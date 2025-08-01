e9acc84cdce7303e702b60cfa3042dee662abab7
Rocketman <abhinayshambharkar1@gmail.com>
2025-08-01 23:54:47 +0530

    model error fix

--- models/User.model.js
+++ models/User.model.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 // schema
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type:String, required: true, unique: true},
-    age: {type:Number, required : true, min: 20, max: 120},
-    location: String,
-    isMarried: Boolean,
-    gender:{type:String, enum:["male","female"]},
-    password: {type:String, default: "pass123"}
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  age: { type: Number, required: true, min: 20, max: 120 },
+  location: String,
+  isMarried: Boolean,
+  gender: { type: String, enum: ["male", "female"] },
+  password: { type: String, default: "pass123" },
 });
 
 //model
-const UserModel = mongoose.model("User",userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
 module.exports = UserModel;
